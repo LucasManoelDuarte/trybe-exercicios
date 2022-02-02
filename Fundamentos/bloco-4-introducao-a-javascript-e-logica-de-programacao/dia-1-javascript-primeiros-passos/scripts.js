2e8307ade8bc605c68f82edfc226ccb0010140fa
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-02 19:14:31 -0300

    Resolve primeiro exercicio - 5 operaçoes aritmeticas

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,17 @@
+const num1 = 32;
+const num2 = 2;
+
+let soma = num1 + num2;
+console.log(soma);
+
+let subtrai = num1 - num2;
+console.log(subtrai);
+
+let multiplica = num1 * num2;
+console.log(multiplica);
+
+let divide = num1 / num2;
+console.log(divide);
+
+let modulo = num1 % num2 === 0;
+console.log(modulo);
