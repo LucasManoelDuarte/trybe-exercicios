299100d9f464b2459a9ee8496437482ebe93bd15
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-02 19:20:54 -0300

    Resolve o segundo exercicio - Qual eh o maior numero

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,3 +1,5 @@
+// Exercício 01
+
 const num1 = 32;
 const num2 = 2;
 
@@ -15,3 +17,20 @@ console.log(divide);
 
 let modulo = num1 % num2 === 0;
 console.log(modulo);
+
+// Exercício 02
+
+const valor1 = 10;
+const valor2 = 8;
+let maior = null;
+
+if (valor1 > valor2) {
+    maior = valor1;
+    console.log("Entre " + valor1 + " e " + valor2 + " o maior é: " + maior);
+} else if (valor1 < valor2) {
+    maior = valor2;
+    console.log("Entre " + valor1 + " e " + valor2 + " o maior é: " + maior);
+} else {
+    console.log("Os valores são iguais");
+}
+
