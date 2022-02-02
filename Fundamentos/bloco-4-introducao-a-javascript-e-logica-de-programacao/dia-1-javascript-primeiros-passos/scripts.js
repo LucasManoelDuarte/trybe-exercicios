273a376c7bd38862500d9fc34ac1824bfb81f9c3
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-02 20:39:07 -0300

    Resolvendo exercicio 08 - Um numero par pelo menos

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -138,3 +138,15 @@ if (nota >= 90 && nota <= 100) {
     console.log("Nota inválida");
 }
 
+// Exercício 08
+
+const num1 = 11;
+const num2 = 7;
+const num3 = 91;
+
+if (num1 % 2 === 0 || num2 % 2 ===0 || num3 % 2 === 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
