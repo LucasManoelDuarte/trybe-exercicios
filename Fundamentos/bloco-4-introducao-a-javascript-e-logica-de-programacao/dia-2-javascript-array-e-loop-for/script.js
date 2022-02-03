c84f9916d31605cf9bab553ec8345335dd073901
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-03 15:58:51 -0300

    Resolve os exercicios 7, 8 e 9 - Menor valor do array, criar um array com o for e dividir os valores por 2

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,14 +1,22 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
-let media = 0;
-let maior = 0;
-let impares = 0;
+let resultado = null;
+let media = null;
+let maior = numbers[0];
+let menor = numbers[0];
+let impares = null;
+let oneToTwentyFive = [];
+let resultadoDivisao = null;
 
 for (let i = 0; i < numbers.length; i += 1) {
+
     resultado += numbers[i];
 
-    if (maior < numbers[i]) {
+    if (numbers[i] > maior) {
         maior = numbers[i];
+    }
+
+    if (numbers[i] < menor) {
+        menor = numbers[i];
     }
 
     if (numbers[i] % 2 != 0) {
@@ -33,6 +41,7 @@ if (media > 20) {
 console.log("A média de todos os valores dentro do array numbers: " + media);
 
 console.log("O maior valor dentro do array numbers: " + maior);
+console.log("O menor valor dentro do array numbers: " + menor);
 
 if (impares === 0) {
     console.log("Nenhum valor ímpar encontado");
@@ -40,3 +49,16 @@ if (impares === 0) {
     console.log("A quantidade de valores ímpares encontrados dentro do array numbers: " + impares);
 }
 
+console.log("");
+
+for (let i = 1; i <= 25; i += 1) {
+    oneToTwentyFive.push(i);
+}
+
+console.log(oneToTwentyFive);
+
+for (number of oneToTwentyFive) {
+    resultadoDivisao = number / 2;
+
+    console.log(number + " / 2 = " + resultadoDivisao);
+}
