cf8c921b7c67f32515c76e7e97bb0b357d461dbc
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-03 15:22:25 -0300

    Resolve os primeiros tres exercicios - Exibir todos os valores do array, exibir a soma e a media de todso os valores

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,2 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+let media = 0;
 
+for (let i = 0; i < numbers.length; i += 1) {
+    resultado += numbers[i];
+
+    console.log(numbers[i]);
+}
+
+media = resultado / numbers.length;
+
+console.log("A soma de todos os valores dentro do array numbers: " + resultado);
+console.log("A média de todos os valores dentro do array numbers: " + media);
