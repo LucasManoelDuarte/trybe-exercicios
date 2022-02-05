d51c23bcc9eaae6b27e7258e172be16f2c32b49e
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-04 21:57:08 -0300

    Resolvendo exercicio 2 (bonus) - Imprimir um triangulo retangulo

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scriptBonus.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scriptBonus.js
@@ -21,3 +21,25 @@ for (let i = 1; i <= valor; i += 1) {
 for (let i = 1; i <= valor; i += 1) {
     console.log(inputLIne);
 }
+
+/*
+    Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+    n = 5
+
+    *
+    **
+    ***
+    ****
+    *****
+*/
+
+let valor = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let i = 0; i <= valor; i += 1) {
+    console.log(inputLine);
+    inputLine += symbol;
+}
+
