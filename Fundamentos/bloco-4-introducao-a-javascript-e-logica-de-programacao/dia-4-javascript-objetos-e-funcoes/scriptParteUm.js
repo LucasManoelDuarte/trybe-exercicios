ddf7f9d1adc12bc706abb870e201b31db7c77316
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-07 19:19:51 -0300

    Resolve o exercicio 5

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scriptParteUm.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scriptParteUm.js
@@ -47,5 +47,59 @@ for (let key in info) {
 }
 
 /*
-    
+    5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+*/
+
+console.log('');
+
+let secondInfo = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell`s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+console.log(`${info.personagem} e ${secondInfo.personagem}`);
+console.log(`${info.origem} e ${secondInfo.origem}`);
+console.log(`${info.nota} e ${secondInfo.nota}`);
+if (info.recorrente === secondInfo.recorrente) {
+    console.log('Ambos recorrentes');
+} else {
+    console.log(`${info.recorrente} e ${secondInfo.recorrente}`);
+};
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
+
+/*
+    Usando o objeto abaixo, faça os exercícios a seguir:
+
+    let leitor = {
+        nome: 'Julia',
+        sobrenome: 'Pessoa',
+        idade: 21,
+        livrosFavoritos: [
+            {
+                titulo: 'O Pior Dia de Todos',
+                autor: 'Daniela Kopsch',
+                editora: 'Tordesilhas',
+            },
+        ],
+    };
+*/
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+    ],
+};
+
+/*
+    1 - 
 */
