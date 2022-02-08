d87e77f9bb7920957cc5da833d40ead214ff2306
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-07 21:31:05 -0300

    Finaliza os exercicios da parte dois (funçoes)

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scriptParteDois.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scriptParteDois.js
@@ -29,13 +29,13 @@ console.log(verificaPalindrome(palavra));
     2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 */
 
-let numbers = [2, 3, 6, 9, 15, 5];
-let maior = numbers[0];
+let numeros = [2, 3, 6, 9, 15, 5];
+let maior = numeros[0];
 let indice = 0;
 
 function verificaMaiorValor(array) {
     for (let i = 0; i < array.length; i += 1) {
-        if (maior < array[i]) {
+        if (array[i] > maior) {
             maior = array[i];
             indice = i;
         };
@@ -44,8 +44,109 @@ function verificaMaiorValor(array) {
     return indice;
 }
 
-console.log(verificaMaiorValor(numbers));
+console.log(verificaMaiorValor(numeros));
 
 /*
-    
+    3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 */
+
+let novosNumeros = [5, 8, 78, 2, 3, 9, 14];
+let menor = novosNumeros[0];
+let indiceMenor = 0;
+
+function verificaMenorValor(array) {
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i] < menor) {
+            menor = array[i];
+            indiceMenor = i;
+        }
+    }
+
+    return indiceMenor;
+}
+
+console.log(verificaMenorValor(novosNumeros));
+
+/*
+    4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+*/
+
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+let maiorNome = nomes[0];
+
+function verificarMaiorNome(array) {
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i].length > maiorNome.length) {
+            maiorNome = array[i];
+        }
+    }
+
+    return maiorNome;
+}
+
+console.log(verificarMaiorNome(nomes));
+
+/*
+    5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+*/
+
+let repeteNumeros = [2, 5, 7, 8, 9, 32, 10, 7, 32, 7];
+let repete = 0;
+let maiorRepeticao = 0;
+let valorRepetido = 0;
+
+function verificaNumeroRepetido(array) {
+    for (let i = 0; i < array.length; i += 1) {
+        let numeroVerificador = array[i];
+        repete = 0;
+
+        for (let j = 0; j < array.length; j += 1) {
+            if (array[j] === numeroVerificador) {
+                repete += 1;
+            }
+        }
+
+        if (repete > maiorRepeticao) {
+            maiorRepeticao = repete;
+            valorRepetido = array[i];
+        }
+    }
+
+    return valorRepetido;
+}
+
+console.log(verificaNumeroRepetido(repeteNumeros));
+
+/*
+    6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+*/
+
+let valor = 7;
+let somatorio = 0;
+
+function somaTodosOsNumeros(number) {
+    for (let i = 1; i <= number; i += 1) {
+        somatorio += i;
+    };
+
+    return somatorio;
+};
+
+console.log(somaTodosOsNumeros(valor));
+
+/*
+    7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word.
+*/
+
+let palavraNormal = 'apartamento';
+let finalDaPalavra = 'sa';
+
+function verificaFinalDaString(word, wordEnding) {
+    if (word.endsWith('sa')) {
+        return true;
+    }
+
+    return false;
+}
+
+console.log(verificaFinalDaString(palavraNormal, finalDaPalavra));
