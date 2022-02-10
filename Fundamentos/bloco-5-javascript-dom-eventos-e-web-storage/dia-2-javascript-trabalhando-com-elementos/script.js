f56e9fbc100454829bc167561cfa2d5b596780e2
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-10 20:06:33 -0300

    Adiciona classes as tags h3 e a tag h1

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -80,3 +80,29 @@ for (let i = 1; i <= 3; i += 1) {
     let subtitle = document.createElement('h3');
     main.appendChild(subtitle);
 }
+
+/*-----------------------------------------------------------------*/
+
+/*
+    10 - Adicione a classe title na tag h1 criada;
+
+    11 - Adicione a classe description nas 3 tags h3 criadas;
+
+    12 - Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+
+    13 - Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+
+    14 - Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+
+    15 - Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+*/
+
+//Exercício 10
+title.className = 'title';
+
+//Exercício 11
+let subtitles = document.getElementsByTagName('h3');
+
+for (let i = 0; i < subtitles.length; i += 1) {
+    subtitles[i].className = 'description';
+}
