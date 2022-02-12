2f0daa89ea52ba15344219d6b560ec941e7dfd4c
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-02-12 17:42:17 -0300

    Adiciona a div task de forma dinamica

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -178,3 +178,23 @@ function tasks(tarefa) {
 }
 
 tasks('Cozinhar');
+
+/*
+    Exercícios 08
+
+    Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+
+     - O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+     - O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+*/
+
+let divTask = document.createElement('div');
+
+function createTaskDescription(cor) {
+    divTask.style.backgroundColor = cor;
+    divTask.className = 'task';
+
+    myTasks.appendChild(divTask);
+}
+
+createTaskDescription('orange');
