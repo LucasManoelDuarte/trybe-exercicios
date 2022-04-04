b121c4c09a65b790c0bcee079c1beeecc1830446
Lucas Duarte <manoel.d.lucas@gmail.com>
2022-04-04 19:12:06 -0300

    Adiciona uma lista de tarefas com o React

--- Front-end/blobo-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
+++ Front-end/blobo-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
@@ -1,5 +1,17 @@
+const taskList = ['Enviar extratos', 'Realizar exercícios', 'Lavar a louça', 'Jogar Persona 5'];
+
+const Task = (list) => {
+  return (
+    <ol>
+      {
+        list.map((item) => <li>{item}</li>)
+      }
+    </ol>
+  );
+}
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  return Task(taskList);
 }
 
 export default App;
